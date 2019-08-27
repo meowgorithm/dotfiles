d0bd34f29e0ab411a05cb1a1f53b8067ea3b3d00
Christian Rocha <christian@rocha.is>
2019-08-27 13:18:11 -0400

    Fish: direnv integration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,6 @@ set -x FZF_DEFAULT_OPTS '--margin=0,2,0,2 --height=20 --inline-info --border'
 bind \cr '__fzf_reverse_isearch'
 
 set -x PATH $PATH:/Users/christian/.local/bin
+
+# Direnv
+direnv hook fish | source
