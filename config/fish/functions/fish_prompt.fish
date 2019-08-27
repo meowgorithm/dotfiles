1158dbff852e97057f03bfbab1460bc526a38220
Christian Rocha <christian@rocha.is>
2019-08-27 13:48:18 -0400

    Fish: fix a spacing issue with error code in the prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -99,7 +99,7 @@ function fish_prompt --description 'Write out the prompt'
 
     if not test $last_status -eq 0
         set_color $fish_color_error
-        echo -n "[$last_status] "
+        echo -n " [$last_status]"
         set_color normal
     end
 
