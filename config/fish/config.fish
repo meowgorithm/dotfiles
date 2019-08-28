5622c11d4d7213cd602f6162043ce2fcfe250a43
Christian Rocha <christian@rocha.is>
2019-08-27 21:26:30 -0400

    Kitty completion in Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,5 @@ set -x FZF_DEFAULT_OPTS '--margin=0,2,0,2 --height=20 --inline-info --border'
 bind \cr '__fzf_reverse_isearch'
 
 set -x PATH $PATH:/Users/christian/.local/bin
+
+kitty + complete setup fish | source
