3ed7e48c99f61a9d9ddce5b31cb6a1fa0789e6a1
Christian Rocha <christian@rocha.is>
2019-09-24 06:01:39 -0400

    Go config for Fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,5 +4,10 @@ bind \cr '__fzf_reverse_isearch'
 
 set -x PATH $PATH:/Users/christian/.local/bin
 
+set -x GOPATH $HOME/.go
+mkdir -p $GOPATH/{src,bin}
+set -x GOBIN $GOPATH/bin
+set -x PATH $PATH:/usr/local/opt/go/libexec/bin:$GOBIN:(go env GOPATH)
+
 # Direnv
 direnv hook fish | source
