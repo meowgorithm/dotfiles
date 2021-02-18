3b2e2ef4ed4d743f8ca4e84fb84d7cec2f8c4816
Christian Rocha <christian@rocha.is>
2021-02-18 11:32:52 -0500

    XMonad keybindings for cycling through windows

--- voidlinux/xmonad/xmonad.hs
+++ voidlinux/xmonad/xmonad.hs
@@ -6,6 +6,10 @@ import XMonad.Util.EZConfig ( additionalKeys )
 import XMonad.Util.Run ( spawnPipe )
 import XMonad.Actions.SpawnOn ( spawnHere )
 import qualified Data.Map as Map
+import qualified XMonad.Actions.CycleWS as CycleWS
+import qualified XMonad.StackSet as W
+import XMonad.Util.WorkspaceCompare ( getSortByIndex )
+import XMonad.Util.NamedScratchpad ( namedScratchpadFilterOutWorkspace )
 
 main :: IO ()
 main =
@@ -30,4 +34,20 @@ myKeys :: (XConfig Layout -> Map.Map (ButtonMask, KeySym) (X ()))
 myKeys conf@XConfig {XMonad.modMask = modMask} = Map.fromList
     [ ((mod4Mask, xK_p), spawn "rofi -show run")
     , ((mod4Mask .|. shiftMask, xK_s), spawnHere "fn=$HOME/screens/$(date +%m-%d-%Y-%H-%M.png); sel=$(slop -f \"-g %g\"); shotgun $sel $fn; Thunar $HOME/screens")
+
+    -- Cycle through non-empty workspaces
+    , ((modMask, xK_bracketright), windows . W.greedyView =<< CycleWS.findWorkspace getSortByIndexNoNSP Next CycleWS.HiddenNonEmptyWS 1)
+    , ((modMask, xK_bracketleft), windows . W.greedyView =<< CycleWS.findWorkspace getSortByIndexNoNSP Prev CycleWS.HiddenNonEmptyWS 1)
+
+    -- Cycle through all workspaces
+    , ((modMask .|. controlMask, xK_bracketright),  CycleWS.nextWS)
+    , ((modMask .|. controlMask, xK_bracketleft ),  CycleWS.prevWS)
+    , ((modMask, xK_z), CycleWS.toggleWS)
+
+    -- Move window to next/previous workspace
+    , ((modMask .|. shiftMask, xK_bracketright), CycleWS.shiftToNext >> CycleWS.nextWS)
+    , ((modMask .|. shiftMask, xK_bracketleft), CycleWS.shiftToPrev >> CycleWS.prevWS)
     ]
+
+    where
+        getSortByIndexNoNSP = fmap (.namedScratchpadFilterOutWorkspace) getSortByIndex
