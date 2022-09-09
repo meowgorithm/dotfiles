b5d19cc6d827ceb9f6b75a53bc3ba486b4f827aa
Christian Rocha <christian@rocha.is>
2022-09-09 13:24:47 -0400

    Add headless Linux Nix target

--- flake.nix
+++ flake.nix
@@ -16,43 +16,68 @@
   } @ inputs: let
     lib = nixpkgs.lib;
 
-    mkHome = system: let
+    mkHome = {
+      name,
+      system,
+      default,
+    }: let
       pkgs = inputs.nixpkgs.legacyPackages."${system}";
       isDarwin = pkgs.stdenv.isDarwin;
-    in {
-      homeManagerConfigurations."${system}" = homeManager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules =
-          [
-            rec {
-              home.stateVersion = "22.11";
-              home.username = "christian";
-              home.homeDirectory =
-                if pkgs.stdenv.isDarwin
-                then "/Users/"
-                else "/home/" + home.username;
-            }
-            ./modules/bash
-            ./modules/floskell
-            ./modules/git.nix
-            ./modules/pkgs.nix
-            ./modules/readline
-            ./modules/tmux
-          ]
-          ++ (
-            if isDarwin
-            then []
-            else [./modules/alacritty.nix]
-          );
-      };
-
-      packages."${system}".default = self.homeManagerConfigurations."${system}".activationPackage;
-    };
+    in
+      {
+        homeManagerConfigurations."${name}" = homeManager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules =
+            [
+              rec {
+                home.stateVersion = "22.11";
+                home.username = "christian";
+                home.homeDirectory =
+                  if pkgs.stdenv.isDarwin
+                  then "/Users/"
+                  else "/home/" + home.username;
+              }
+              ./modules/bash
+              ./modules/floskell
+              ./modules/git.nix
+              ./modules/pkgs.nix
+              ./modules/readline
+              ./modules/tmux
+            ]
+            ++ (
+              if name != "headless"
+              then [./modules/alacritty.nix]
+              else []
+            );
+        };
+      }
+      // (
+        if default
+        then {packages."${system}".default = self.homeManagerConfigurations."${name}".activationPackage;}
+        else {}
+      );
 
     systems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
+      {
+        name = "linux";
+        system = "x86_64-linux";
+        default = true;
+      }
+      {
+        name = "macOS-intel";
+        system = "x86_64-darwin";
+        default = true;
+      }
+      {
+        name = "macOS-arm";
+        system = "aarch64-darwin";
+        default = true;
+      }
+      {
+        name = "headless";
+        system = "x86_64-linux";
+        default = false;
+      }
     ];
   in
     lib.foldr lib.recursiveUpdate {} (map mkHome systems);
