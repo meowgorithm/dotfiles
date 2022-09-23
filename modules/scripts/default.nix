a61c7dfd7bdd37ea9969ec0b38e0840498993452
Christian Rocha <christian@rocha.is>
2022-09-23 10:11:05 -0400

    Move remaining scripts into Nix

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -8,8 +8,12 @@
     {
       ".bin/install-docker".source = ./install-docker;
       ".bin/make-vid".source = ./make-vid;
+      ".bin/sessions".source = ./sessions;
+      ".bin/sessions.gpg".source = ./sessions.gpg;
       ".bin/tm".source = ./tm;
       ".bin/tmls".source = ./tmls;
+      ".bin/webp-jpeg".source = ./webp-jpeg;
+      ".bin/webp-png".source = ./webp-png;
     }
     // (
       if pkgs.stdenv.isDarwin
