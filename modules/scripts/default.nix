cc6093229a127f84819f5db8b94c3ab647708720
Christian Rocha <christian@rocha.is>
2022-09-22 20:16:59 -0400

    Move Linux scripts into Nix

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -4,10 +4,20 @@
   lib,
   ...
 }: {
-  home.file = {
-    ".bin/install-docker".source = ./install-docker;
-    ".bin/make-vid".source = ./make-vid;
-    ".bin/tm".source = ./tm;
-    ".bin/tmls".source = ./tmls;
-  };
+  home.file =
+    {
+      ".bin/install-docker".source = ./install-docker;
+      ".bin/make-vid".source = ./make-vid;
+      ".bin/tm".source = ./tm;
+      ".bin/tmls".source = ./tmls;
+    }
+    // (
+      if pkgs.stdenv.isDarwin
+      then {}
+      else {
+        ".bin/map-keys".source = ./map-keys;
+        ".bin/setup-mouse".source = ./setup-mouse;
+        ".bin/setup-wacom".source = ./setup-wacom;
+      }
+    );
 }
