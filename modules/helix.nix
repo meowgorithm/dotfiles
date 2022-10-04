4f2a34e86e1ae3f7f1e79ac4a53f185872d7a635
Christian Rocha <christian@rocha.is>
2022-10-04 12:13:47 -0400

    Simple Helix config

--- modules/helix.nix
+++ modules/helix.nix
@@ -6,5 +6,20 @@
 }: {
   programs.helix = {
     enable = true;
+
+    settings = {
+      keys.normal = {
+        g.q = ":reflow";
+        X = "extend_line_above";
+      };
+    };
+
+    languages = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter = {command = "alejandra";};
+      }
+    ];
   };
 }
