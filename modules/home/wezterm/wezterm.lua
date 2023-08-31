e3fdc39773ae29ca6fc2aa8792b4bdd52da2f9f6
Christian Rocha <christian@rocha.is>
2023-08-31 19:59:24 -0400

    Tighten Wezterm tracking

--- modules/home/wezterm/wezterm.lua
+++ modules/home/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.font = wezterm.font({
 })
 config.font_size = 13.0
 config.line_height = 1.0
-config.cell_width = 0.95
+config.cell_width = 0.90
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.color_scheme = "Pantera Negra"
