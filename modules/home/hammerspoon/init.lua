18d4c28ec6c56941954fb0689848794f2c6295e9
Christian Rocha <christian@rocha.is>
2023-09-07 12:05:19 -0400

    Hide AE Render Engine in Hammerspoon chooser

--- modules/home/hammerspoon/init.lua
+++ modules/home/hammerspoon/init.lua
@@ -126,7 +126,7 @@ do
 	local excludes = {
 		".",
 		"..",
-		"After Effects Render Engine 2023.app",
+		"Adobe After Effects Render Engine 2023.app",
 		"Boot Camp Assistant.app",
 		"Cinema 4D Team Render Client.app",
 		"Cinema 4D Team Render Server.app",
