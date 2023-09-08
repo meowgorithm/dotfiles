ebd86ca433a692ac4087dde287736cf80d65142a
Christian Rocha <christian@rocha.is>
2023-09-08 15:23:42 -0400

    Fix Wezterm mouse sizing on Linux

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -1,4 +1,4 @@
-{...}: let
+{pkgs, ...}: let
   colors = import ../colors.nix;
 in {
   programs.wezterm = {
@@ -30,6 +30,49 @@ in {
         ];
       };
     };
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig =
+      ({
+        fontSize,
+        cellWidth,
+      }: ''
+        return (function()
+        	local wezterm = require("wezterm")
+        	local config = {}
+
+        	if wezterm.config_builder then
+        		config = wezterm.config_builder()
+        	end
+
+        	config.font = wezterm.font({
+        		family = "JetBrains Mono",
+        		weight = "Medium",
+        	})
+        	config.font_size = ${fontSize}
+        	config.line_height = 1.0
+        	config.cell_width = ${cellWidth}
+        	config.hide_tab_bar_if_only_one_tab = true
+        	config.window_decorations = "RESIZE"
+        	config.color_scheme = "Pantera Negra"
+        	config.window_padding = {
+        		left = "16pt",
+        		right = "16pt",
+        		top = "12pt",
+        		bottom = "12pt",
+        	}
+
+        	return config
+        end)()
+      '')
+      (
+        if pkgs.stdenv.isLinux
+        then {
+          fontSize = "9.5";
+          cellWidth = "1.0";
+        }
+        else {
+          fontSize = "13.0";
+          cellWidth = "0.9";
+        }
+      );
   };
 }
