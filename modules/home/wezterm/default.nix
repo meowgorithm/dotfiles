61f4495654b894b78864292c62b87cc16c77d884
Christian Rocha <christian@rocha.is>
2024-03-25 21:10:50 -0400

    Revert "Re-add Wezterm on macOS (the nix package seems to work again)"
    This reverts commit 725004d56e35f8837a2e5c2794171d602a09fd4f.

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -2,7 +2,7 @@
   colors = import ../colors.nix;
 in {
   programs.wezterm = {
-    enable = true;
+    enable = !pkgs.stdenv.isDarwin;
     enableBashIntegration = true;
     colorSchemes = {
       "Pantera Negra" = {
