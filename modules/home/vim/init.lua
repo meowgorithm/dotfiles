14a9ecd7a748aa9c82574a000c3a3909400a02f5
Christian Rocha <christian@rocha.is>
2024-08-26 10:37:28 -0400

    Correct misplaced treesitter setting

--- modules/home/vim/init.lua
+++ modules/home/vim/init.lua
@@ -236,9 +236,8 @@ end
 
 -- TreeSitter
 require("nvim-treesitter.configs").setup({
-	highlight = { enable = true },
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
 	indent = { enable = true },
-	additional_vim_regex_highlighting = false,
 })
 
 -- Vsnip
