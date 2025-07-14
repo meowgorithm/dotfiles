d459cd5a64a24271fe00074f9c2cddf159b9b7a9
Christian Rocha <christian@rocha.is>
2025-07-14 13:09:18 -0400

    Move more packages from Nix to Homebrew

--- modules/home/pkgs.nix
+++ modules/home/pkgs.nix
@@ -1,30 +1,20 @@
 {
   pkgs,
-  lib,
   charmPkgs,
   carlosPkgs,
-  headless,
   ...
-}: let
-  isLinux = pkgs.stdenv.isLinux;
-  isDarwin = pkgs.stdenv.isDarwin;
-  # isX86_64 = pkgs.stdenv.isx86_64;
-in {
+}: {
   home.packages = with pkgs;
     [
       cargo
       clang
       coreutils
       ctags
-      doctl
       getopt
-      gh
       gnumake
       gnupg
       gnused
       gnutar
-      go-task
-      haskellPackages.cabal-fmt
       htop
       imagemagick
       librsvg # for the rsvg-convert binary
@@ -33,8 +23,6 @@ in {
       moreutils
       python3
       tree-sitter
-      ttyd
-      zlib
     ]
     # Haskell
     ++ (with pkgs.haskellPackages; [
@@ -45,44 +33,10 @@ in {
       elm
       elm-language-server
     ])
-    # Rust
-    ++ (with pkgs; [
-      rust-analyzer
-      rustc
-    ])
     # Various LSP
     ++ (with pkgs; [
-      lua-language-server
       nil
-      #semgrep # broken in nixpkgs unstable
-      stylua
-      typescript
-      typescript-language-server
-      vim-language-server
-      vscode-langservers-extracted
-      yaml-language-server
     ])
-    # LSPs that use Node
-    ++ (with pkgs.nodePackages_latest; [
-      prettier
-      svgo
-    ])
-    # Desktops only
-    ++ (
-      lib.optionals (! headless) (with pkgs; [
-        vscode
-      ])
-    )
-    # Linux Desktop
-    ++ (lib.optionals (isLinux && ! headless) (with pkgs; [
-      element
-    ]))
-    # macOS
-    ++ (lib.optionals isDarwin (with pkgs; [
-      cachix
-      dozer
-      monitorcontrol
-    ]))
     # Charm NUR
     ++ (map (x: pkgs.${x}) charmPkgs)
     # Carlos' NUR
