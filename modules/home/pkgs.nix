2b1c03340f6fb4fb9a9a45fc3e7e08402a854ba5
Christian Rocha <christian@rocha.is>
2025-09-01 15:36:09 -0400

    Move z.lua init to bashrc on macOS

--- modules/home/pkgs.nix
+++ modules/home/pkgs.nix
@@ -10,19 +10,15 @@ in {
   home.packages = with pkgs;
   # macOS
     (lib.optionals isDarwin (with pkgs; [
-      vim-language-server
-      elm-language-server
-      nil
       cachix
       dozer
+      elmPackages.elm-language-server
       monitorcontrol
+      nil
+      vim-language-server
     ]))
     # Charm NUR
     ++ (map (x: pkgs.${x}) charmPkgs)
     # Carlos' NUR
     ++ (map (x: pkgs.${x}) carlosPkgs);
-
-  programs = {
-    z-lua.enable = true;
-  };
 }
